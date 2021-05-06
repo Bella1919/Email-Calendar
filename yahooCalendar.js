018a62147756e5b4d87918d64f7a02ad93da90d0
Bella D <bella14378899@gmail.com>
2021-05-07 00:11:23 +1200

    change parameter

--- yahooCalendar.js
+++ yahooCalendar.js
@@ -3,14 +3,15 @@ var meetingDetail={
     url:"https://calendar.yahoo.com/?",
     v:"60",
     //path require must be 60
-    title:"meeting",
+    TITLE:"meeting",
     //rru:require text
-    ST:"20210524T173000Z",
+    ST:"20210524T173000000Z",
     //  Event start time in UTC. Will be converted to the user's time zone.format:yyyymmddThhmmss 
     //eg:May 24,2021 19:30 UTC+02:00(Europe/Paris)-->20210524T173000Z
-    ET:"20210524T180000Z",
+    ET:"20210524T180000000Z",
     // Event end time in UTC. Will be converted to the user's time zone.format:yyyymmddThhmmss
     //eg:May 24,2021 20:00 UTC+02:00(Europe/Paris)-->20210524T180000Z
+    DESC:"test meeting"
 }
 
 
@@ -18,7 +19,7 @@ var meetingDetail={
 this.addToCalendar()
 function addToCalendar(){
     
-    let api = `${meetingDetail.url}&v=${meetingDetail.v}&ST=${meetingDetail.ST}&ET=${meetingDetail.ET}&title=${meetingDetail.title}`
+    let api = `${meetingDetail.url}&v=${meetingDetail.v}&ST=${meetingDetail.ST}&ET=${meetingDetail.ET}&TITLE=${meetingDetail.TITLE}&DESC=${meetingDetail.DESC}`
 
 
    let aObj =  document.getElementById("addYahooCalendar")
